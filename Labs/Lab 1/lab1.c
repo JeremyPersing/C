d69ddb4b949690b5d7c04320acd2e0851111a4c4
Jeremy Persing <jpersing1999#gmail.com>
2020-04-12 15:31:19 -0700

    Lab 1

---
+++ Labs/Lab 1/lab1.c
@@ -0,0 +1,18 @@
+/*--------------*/
+/*Jeremy Persing*/
+/*Lab 1         */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    printf("\nLab 1.\n\n");
+    printf("Hello World.\n");
+    printf("Hi, Jeremy Persing.\n");
+    printf("\nThe harder the conflict, the greater the triumph.\n- George Washington\n\n");
+   
+    return EXIT_SUCCESS;
+}
+/*--------------*/
